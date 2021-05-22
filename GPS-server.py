df8bb316623ac80045bf7057adafc6063f601a4b
Papalight <theo.papafotiou@gmail.com>
2021-05-23 01:36:50 +0300

    GPS server updated with ArUco codes
    Codes added on the corners of the track and on the car

--- GPS-server.py
+++ GPS-server.py
@@ -1,7 +1,10 @@
 import cv2
+import sys
 import numpy as np
 import time
 import math
+
+from numpy.core.fromnumeric import shape
 from detect_motion import objectTracking
 from skimage.transform import ProjectiveTransform
 import matplotlib.pyplot as plt
@@ -9,18 +12,20 @@ import matplotlib.pyplot as plt
 tracker = objectTracking()
 transformer = ProjectiveTransform()
 size_of_track_w = 100 #cm
-size_of_track_h = 100 #cm
+size_of_track_h = 40 #cm
 
 qrCodeDetector = cv2.QRCodeDetector()
-#cap = cv2.VideoCapture(0)
+arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
+arucoParams = cv2.aruco.DetectorParameters_create()
+cap = cv2.VideoCapture(1)
 
 def detect_qr(image, mask):
     masked = cv2.bitwise_and(image, image, mask=mask)
     decodedText, points, _ = qrCodeDetector.detectAndDecode(masked)
-    print('\n', points)
+    print('\n', points, '\n')
     return points
 
-def center_of_points(points):
+def center_of_QR(points):
     h1_c = abs(points[0][3][1] - points[0][0][1])/2
     w1_c = abs(points[0][1][0] - points[0][0][0])/2
     #h1_c = math.dist(points[0], points[3])/2
@@ -30,48 +35,109 @@ def center_of_points(points):
     print("Centers are: ", center_x, center_y)
     return center_x, center_y
 
-frame_init = cv2.imread("Test-track/test_track_qr.png")
-#success, frame_init = cap.read()
-#frame_init = cv2.flip(frame_init, 1)
+def detect_ArUco(frame, flag, points):
 
+    correct_detection = False
+    (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, arucoDict, parameters=arucoParams)
+    #print("IDs: ", ids)
+    cX = 0
+    cY = 0
+
+    if len(corners) > 0:
+        # flatten the ArUco IDs list
+        ids = ids.flatten()
+        counter = -1
+
+        # loop over the detected ArUCo corners
+        for (markerCorner, markerID) in zip(corners, ids):
+            if flag == 0 and markerID != 8 and markerID != 0:
+                counter += 1
+                # marker corners are always returned in top-left, top-right, bottom-right and bottom-left order
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
+                
+                # convert each of the (x, y)-coordinate pairs to integers
+                topRight = (int(topRight[0]), int(topRight[1]))
+                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+                cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+                cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+                cv2.circle(frame, (int(cX), int(cY)), radius=5, color=(0, 0, 255), thickness=-1)
+                cv2.putText(frame, str(markerID), (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+                points[counter] = (cX, cY, markerID)
+
+                if counter == 3:
+                    correct_detection = True
+            
+            elif flag == 1 and markerID == 8:
+                counter += 1
+                # marker corners are always returned in top-left, top-right, bottom-right and bottom-left order
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
+                
+                # convert each of the (x, y)-coordinate pairs to integers
+                topRight = (int(topRight[0]), int(topRight[1]))
+                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+                cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+                cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+                cv2.circle(frame, (int(cX), int(cY)), radius=5, color=(0, 0, 255), thickness=-1)
+                cv2.putText(frame, str(markerID), (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+                points = np.zeros(shape=(5,3))
+                points[counter] = (cX, cY, markerID)
+
+                if cX > 0 and cY > 0:
+                    correct_detection = True 
+
+    return frame, points, correct_detection
+
+
+#frame_init = cv2.imread("Test-track/test_track_qr.png")
+success, frame_init = cap.read()
+#frame_init = cv2.flip(frame_init, 1)
 height, width, _ = frame_init.shape
 
-mask = np.zeros((4, height, width), dtype="uint8")
-points = np.zeros((4, 1, 4, 2))
+points = np.zeros((5, 3))
 center_x = np.zeros(4)
 center_y = np.zeros(4)
-# upper left
-cv2.rectangle(mask[0], (0, 0), (width//2, height//2), 255, -1)
-# upper right
-cv2.rectangle(mask[1], (width//2, 0), (width, height//2), 255, -1)
-# down left
-cv2.rectangle(mask[2], (0, height//2), (width//2, height), 255, -1)
-# down right
-cv2.rectangle(mask[3], (width//2, height//2), (width, height), 255, -1)
+bottom_left_track = np.zeros((2))
+bottom_right_track = np.zeros((2))
+top_left_track = np.zeros((2))
+top_right_track = np.zeros((2))
 
-while True:
-    for i in range (0, 4):
-        points[i] = detect_qr(frame_init, mask[i])
-        center_x[i], center_y[i] = center_of_points(points[i])
-        time.sleep(0.5)
-        if center_x[i] == 0 and center_y[i] == 0:
-            print("QR code not detected, WTF")
-            cv2.imwrite("error_qr.png", frame_init)
-            break
-    if (center_x[0] > 0) and (center_x[1] > 0) and (center_x[2] > 0) and (center_x[3] > 0):
-        for i in range (0,4):
-            frame_init = cv2.circle(frame_init, (int(center_x[i]), int(center_y[i])), radius=5, color=(0, 0, 255), thickness=1)
-        cv2.imwrite("correct_init_frame.png", frame_init)
-        break
+time.sleep(1)
+correct_detection = False
+while correct_detection is False:
+    success, frame_init = cap.read()
+    #frame_init = cv2.flip(frame_init, 1)
+    frame_init, points, correct_detection = detect_ArUco(frame_init, flag=0, points=points)
+    print(points)
+    time.sleep(1)
 
-top_left = [center_x[0], center_y[0]]
-top_right = [center_x[1], center_y[1]]
-bottom_left = [center_x[2], center_y[2]]
-bottom_right = [center_x[3], center_y[3]]
+for i in range(0, 4):
+    index = [points[i][0], points[i][1]]
+    if points[i][2] == 1:
+        top_left_track = index
+        print("TL: ", top_left_track)
+    elif points[i][2] == 2:
+        top_right_track = index
+        print("TR: ", top_right_track)
+    elif points[i][2] == 3:
+        bottom_right_track = index
+        print("BR: ", bottom_right_track)
+    elif points[i][2] == 4:
+        bottom_left_track = index
+        print("BL: ", bottom_left_track)
 
 # Transform quadrilateral to rectangle
 src = np.asarray(
-    [bottom_left, top_left, top_right, bottom_right])
+    [bottom_left_track, top_left_track, top_right_track, bottom_right_track])
 
 dst = np.asarray([
                 [(size_of_track_w - size_of_track_h)/2, size_of_track_h],
@@ -82,60 +148,56 @@ dst = np.asarray([
 
 transformer_start = time.time()
 success = transformer.estimate(src, dst)
-print(success)
 transformer_stop = time.time()
 transformer_duration = transformer_stop - transformer_start
 if success:
     print("Duration of the transformation: " + str(transformer_duration))
 
 # Frames initialization
-frame1 = np.zeros((width, height))
-frame2 = np.zeros((width, height))
-frame3 = np.zeros((width, height))
+frame = np.zeros((width, height))
 
-#success, frame1 = cap.read()
-frame1 = cv2.imread("Test-track/"+str(1)+".png")
-cv2.imshow("Before transfrom", frame1)
+success, frame = cap.read()
+#frame1 = cv2.imread("Test-track/"+str(1)+".png")
 
-interval_x = 10*(center_x[1] - center_x[0])/size_of_track_w
-interval_y = 10*(center_y[3] - center_y[0])/size_of_track_h
+interval_x = 10*(top_right_track[0] - top_left_track[0])/size_of_track_w
+interval_y = 10*(bottom_right_track[1] - top_left_track[1])/size_of_track_h
 
 start = time.time()
 count_frames = 0
 
 coord = np.array([])
 
-#while cap.isOpened():
-for j in range (2, 28):
+while cap.isOpened():
+#for j in range (2, 28):
 
-    time.sleep(0.1)
-    frame2 = frame1
-    frame1 = cv2.imread("Test-track/"+str(j)+".png")
-    #success, frame1 = cap.read()
-    frame3, point_x, point_y = tracker.objectTracking(frame1, frame2)
+    time.sleep(0.5)
+    #frame = cv2.imread("Test-track/"+str(j)+".png")
+    success, frame = cap.read()
+    frame, points, correct_detection = detect_ArUco(frame, flag=1, points=points)
+    point_car = points[0]
 
-    # if point_x == 0 and point_y == 0 and (time.time() - start > 80):
-    #     cap.release()
+    if point_car[0] == 0 and point_car[1] == 0 and (time.time() - start > 80):
+        cap.release()
     
-    if point_x > 0 and point_y > 0:
-        gps_x = point_x - center_x[0]
-        gps_y = point_y - center_y[0]
-        actual_gps_x = gps_x * size_of_track_w / (center_x[1] - center_x[0])
-        actual_gps_y = gps_y * size_of_track_h / (center_y[3] - center_y[0])
-        cv2.putText(frame3, "X: "+str(int(actual_gps_x))+" & Y: "+str(int(actual_gps_y)), (int(gps_x), int(gps_y)), cv2.FONT_HERSHEY_SIMPLEX,
+    if point_car[0] > 0 and point_car[1] > 0:
+        gps_x = point_car[0] - top_left_track[0]
+        gps_y = point_car[1] - top_left_track[1]
+        actual_gps_x = gps_x * size_of_track_w / (top_right_track[0] - top_left_track[0])
+        actual_gps_y = gps_y * size_of_track_h / (bottom_right_track[1] - top_left_track[1])
+        cv2.putText(frame, "X: "+str(int(actual_gps_x))+" & Y: "+str(int(actual_gps_y)), (int(gps_x), int(gps_y)), cv2.FONT_HERSHEY_SIMPLEX,
                         1, (0, 0, 255), 3)
 
         
-        for k in range (int(center_x[0]), int(center_x[1]), int(interval_x)):
-            cv2.line(frame3, (k, int(center_y[0])), (k, int(center_y[3])), (255, 0, 0), 1, 1)
+        for k in range (int(top_left_track[0]), int(top_right_track[0]), int(interval_x)):
+            cv2.line(frame, (k, int(top_left_track[1])), (k, int(bottom_right_track[1])), (255, 0, 0), 1, 1)
 
-        for p in range (int(center_y[0]), int(center_y[3]), int(interval_y)):
-            cv2.line(frame3, (int(center_x[0]), p), (int(center_x[1]), p), (255, 0, 0), 1, 1)
+        for p in range (int(top_left_track[1]), int(bottom_right_track[1]), int(interval_y)):
+            cv2.line(frame, (int(top_left_track[0]), p), (int(top_right_track[0]), p), (255, 0, 0), 1, 1)
 
         
-        cv2.imshow("Frames", frame3)
+        cv2.imshow("Frames", frame)
         count_frames += 1
-        cv2.imwrite("test_grid_"+str(count_frames)+".png", frame3)
+        cv2.imwrite("test_grid_"+str(count_frames)+".png", frame)
         print('The GPS coordinates are: X -> ', actual_gps_x, ' || Y -> ', actual_gps_y)
         if count_frames == 1:
             coord = np.append(coord, [gps_x, gps_y])
---
+++ test.py
@@ -0,0 +1,46 @@
+import cv2
+
+cap = cv2.VideoCapture(1)
+arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
+arucoParams = cv2.aruco.DetectorParameters_create()
+
+def detect_ArUco(frame):
+    (corners, ids, rejected) = cv2.aruco.detectMarkers(frame,
+		arucoDict, parameters=arucoParams)
+    print(ids)
+    #print(corners)
+    cX = 0
+    cY = 0
+
+    if len(corners) > 0:
+        # flatten the ArUco IDs list
+        ids = ids.flatten()
+
+        # loop over the detected ArUCo corners
+        for (markerCorner, markerID) in zip(corners, ids):
+            # marker corners are always returned in top-left, top-right, bottom-right and bottom-left order
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            
+            # convert each of the (x, y)-coordinate pairs to integers
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            cv2.circle(frame, (int(cX), int(cY)), radius=5, color=(0, 0, 255), thickness=-1)
+            cv2.putText(frame, str(markerID), (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    return frame, cX, cY
+countFrames = 0
+while 1:
+    success, img = cap.read()
+
+    img, point_x, point_y = detect_ArUco(img)
+    cv2.imshow("Frame", img)
+    
+    countFrames+=1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
