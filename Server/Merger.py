0bd5dfae93a089aa200a4ccff4f5bf240a4f92d8
TheoPapafotiou <theo.papafotiou@gmail.com>
2021-07-10 14:35:33 +0100

    Updated test files

--- Server/Merger.py
+++ Server/Merger.py
@@ -3,14 +3,17 @@ import cv2
 class Merge_Files:
 
     def __init__(self):
-        self.res_width = self.init_width//3
-        self.res_height = self.init_height//3
-        
-        self.coor_width = 215
-        self.coor_height = 312
+        self.coor_width = 215+50+10
+        self.coor_height = 360+20+20
         
         self.init_width = 2304
         self.init_height = 1536
+        
+        self.res_width = self.init_width//3
+        self.res_height = self.init_height//3
+        self.res_size = (self.res_width, self.res_height)
+        self.res_width = 960
+        self.res_height = 540
 
         self.x1 = 0
         self.y1 = 0
@@ -18,7 +21,10 @@ class Merge_Files:
         self.y2 = self.coor_height
 
     def get_merged_frame(self, im1, im2):
-        dst = cv2.imread('1000x800_blank.png')
+        dst = cv2.imread('1500x1000_blank.png')
+        
+#         im1 = cv2.resize(im1, self.res_size, interpolation = cv2.INTER_AREA)
+#         im2 = cv2.resize(im2, self.res_size, interpolation = cv2.INTER_AREA)
         
         dst[self.y1 : self.y1+self.res_height, self.x1 : self.x1+self.res_width] = im1
         dst[self.y2 : self.y2+self.res_height, self.x2 : self.x2+self.res_width] = im2
--- Server/test_camera.py
+++ Server/test_camera.py
@@ -4,18 +4,46 @@ from time import sleep
 from Merger import Merge_Files
 
 Merger = Merge_Files()
-cap0 = cv2.VideoCapture(0)
-cap2 = cv2.VideoCapture(2)
-sleep(4)
 
-for k in range(0, 4):
+def video(id, width, height):
+    cap = cv2.VideoCapture(id)
+    cap.set(3, width)
+    cap.set(4, height)
     
+    return cap
+
+cap0 = video(0, 960, 540)
+print(cap0.get(3))
+print(cap0.get(4))
+sleep(2)
+cap2 = video(2, 960, 540)
+print(cap2.get(3))
+print(cap2.get(4))
+sleep(2)
+
+res_size = (2304//3, 1536//3)
+
+if cap0 is None:
+    print("Error in cap0")
+    cap0.release()
+if cap2 is None:
+    print("Error in cap2")
+    cap2.release()
+
+for k in range(0, 10):
+    sleep(0.1)
     start = time.time()
     _, frame0 = cap0.read()
+#     frame0 = cv2.resize(frame0, res_size, interpolation = cv2.INTER_AREA)
     _, frame2 = cap2.read()
-
+#     frame2 = cv2.resize(frame2, res_size, interpolation = cv2.INTER_AREA)
+    
     merged_frame = Merger.get_merged_frame(frame0, frame2)
     print("Procedure Duration: ", time.time() - start)
 
 
-cv2.imwrite("Merged_frame", merged_frame)
+cv2.imwrite("Merged_frame_Aruco_car5.jpg", merged_frame)
+cv2.imwrite("Frame0.jpg", frame0)
+cv2.imwrite("Frame2.jpg", frame2)
+cap0.release()
+cap2.release()
