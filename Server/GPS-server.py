1dfdbb7bf233eab04589e1105760bf07e397d137
Papalight <theo.papafotiou@gmail.com>
2021-07-10 16:50:35 +0300

    Updated server files

--- Server/GPS-server.py
+++ Server/GPS-server.py
@@ -11,34 +11,17 @@ import matplotlib.pyplot as plt
 
 tracker = objectTracking()
 transformer = ProjectiveTransform()
-size_of_track_w = 100 #cm
-size_of_track_h = 40 #cm
+size_of_track_w = 485#530 #cm
+size_of_track_h = 400 #cm
 
-qrCodeDetector = cv2.QRCodeDetector()
 arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
 arucoParams = cv2.aruco.DetectorParameters_create()
-cap = cv2.VideoCapture(1)
-
-def detect_qr(image, mask):
-    masked = cv2.bitwise_and(image, image, mask=mask)
-    decodedText, points, _ = qrCodeDetector.detectAndDecode(masked)
-    print('\n', points, '\n')
-    return points
-
-def center_of_QR(points):
-    h1_c = abs(points[0][3][1] - points[0][0][1])/2
-    w1_c = abs(points[0][1][0] - points[0][0][0])/2
-    #h1_c = math.dist(points[0], points[3])/2
-    #w1_c = math.dist(points[0], points[1])/2
-    center_x = int(points[0][0][0] + w1_c)
-    center_y = int(points[0][0][1] + h1_c)
-    print("Centers are: ", center_x, center_y)
-    return center_x, center_y
+# cap = cv2.VideoCapture(1)
 
 def detect_ArUco(frame, flag, points):
 
     correct_detection = False
-    (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, arucoDict, parameters=arucoParams)
+    (corners, ids, _) = cv2.aruco.detectMarkers(frame, arucoDict, parameters=arucoParams)
     #print("IDs: ", ids)
     cX = 0
     cY = 0
@@ -50,7 +33,7 @@ def detect_ArUco(frame, flag, points):
 
         # loop over the detected ArUCo corners
         for (markerCorner, markerID) in zip(corners, ids):
-            if flag == 0 and markerID != 8 and markerID != 0:
+            if flag == 0 and markerID != 10:
                 counter += 1
                 # marker corners are always returned in top-left, top-right, bottom-right and bottom-left order
                 corners = markerCorner.reshape((4, 2))
@@ -72,7 +55,7 @@ def detect_ArUco(frame, flag, points):
                 if counter == 3:
                     correct_detection = True
             
-            elif flag == 1 and markerID == 8:
+            elif flag == 1 and markerID == 10:
                 counter += 1
                 # marker corners are always returned in top-left, top-right, bottom-right and bottom-left order
                 corners = markerCorner.reshape((4, 2))
@@ -98,8 +81,8 @@ def detect_ArUco(frame, flag, points):
     return frame, points, correct_detection
 
 
-#frame_init = cv2.imread("Test-track/test_track_qr.png")
-success, frame_init = cap.read()
+frame_init = cv2.imread("Track/Merged_frame_Aruco3.jpg")
+#success, frame_init = cap.read()
 #frame_init = cv2.flip(frame_init, 1)
 height, width, _ = frame_init.shape
 
@@ -114,7 +97,7 @@ top_right_track = np.zeros((2))
 time.sleep(1)
 correct_detection = False
 while correct_detection is False:
-    success, frame_init = cap.read()
+    #success, frame_init = cap.read()
     #frame_init = cv2.flip(frame_init, 1)
     frame_init, points, correct_detection = detect_ArUco(frame_init, flag=0, points=points)
     print(points)
@@ -122,19 +105,31 @@ while correct_detection is False:
 
 for i in range(0, 4):
     index = [points[i][0], points[i][1]]
-    if points[i][2] == 1:
+    if points[i][2] == 6:
         top_left_track = index
         print("TL: ", top_left_track)
-    elif points[i][2] == 2:
+    elif points[i][2] == 7:
         top_right_track = index
         print("TR: ", top_right_track)
-    elif points[i][2] == 3:
+    elif points[i][2] == 9:
         bottom_right_track = index
         print("BR: ", bottom_right_track)
-    elif points[i][2] == 4:
+    elif points[i][2] == 8:
         bottom_left_track = index
         print("BL: ", bottom_left_track)
 
+interval_x = 10*(bottom_right_track[0] - top_left_track[0])/size_of_track_w
+interval_y = 10*(bottom_right_track[1] - top_left_track[1])/size_of_track_h
+
+for k in range (int(top_left_track[0]), int(bottom_right_track[0]), int(interval_x)):
+            cv2.line(frame_init, (k, int(top_left_track[1])), (k, int(bottom_right_track[1])), (255, 0, 0), 1, 1)
+
+for p in range (int(top_left_track[1]), int(bottom_right_track[1]), int(interval_y)):
+    cv2.line(frame_init, (int(top_left_track[0]), p), (int(bottom_right_track[0]), p), (255, 0, 0), 1, 1)
+
+cv2.imwrite("Merged_frame_points.jpg", frame_init)
+
+'''
 # Transform quadrilateral to rectangle
 src = np.asarray(
     [bottom_left_track, top_left_track, top_right_track, bottom_right_track])
@@ -152,52 +147,51 @@ transformer_stop = time.time()
 transformer_duration = transformer_stop - transformer_start
 if success:
     print("Duration of the transformation: " + str(transformer_duration))
+'''
 
 # Frames initialization
 frame = np.zeros((width, height))
 
-success, frame = cap.read()
-#frame1 = cv2.imread("Test-track/"+str(1)+".png")
-
-interval_x = 10*(top_right_track[0] - top_left_track[0])/size_of_track_w
-interval_y = 10*(bottom_right_track[1] - top_left_track[1])/size_of_track_h
+#success, frame = cap.read()
+frame1 = cv2.imread("Track/Merged_frame_Aruco_car"+str(1)+".jpg")
 
 start = time.time()
 count_frames = 0
 
 coord = np.array([])
+x_offset = 0
 
-while cap.isOpened():
-#for j in range (2, 28):
+#while cap.isOpened():
+for j in range (1, 6):
 
     time.sleep(0.5)
-    #frame = cv2.imread("Test-track/"+str(j)+".png")
-    success, frame = cap.read()
+    frame = cv2.imread("Track/Merged_frame_Aruco_car"+str(j)+".jpg")
+    #success, frame = cap.read()
     frame, points, correct_detection = detect_ArUco(frame, flag=1, points=points)
     point_car = points[0]
 
-    if point_car[0] == 0 and point_car[1] == 0 and (time.time() - start > 80):
-        cap.release()
+    # if point_car[0] == 0 and point_car[1] == 0 and (time.time() - start > 80):
+    #     cap.release()
     
     if point_car[0] > 0 and point_car[1] > 0:
-        gps_x = point_car[0] - top_left_track[0]
+        gps_x = point_car[0] - (top_left_track[0])
         gps_y = point_car[1] - top_left_track[1]
-        actual_gps_x = gps_x * size_of_track_w / (top_right_track[0] - top_left_track[0])
-        actual_gps_y = gps_y * size_of_track_h / (bottom_right_track[1] - top_left_track[1])
+        actual_gps_x = x_offset + gps_x * size_of_track_w / ((bottom_right_track[0]) - (top_left_track[0]))
+        actual_gps_y = gps_y * size_of_track_h / ((bottom_right_track[1]) - top_left_track[1])
         cv2.putText(frame, "X: "+str(int(actual_gps_x))+" & Y: "+str(int(actual_gps_y)), (int(gps_x), int(gps_y)), cv2.FONT_HERSHEY_SIMPLEX,
                         1, (0, 0, 255), 3)
 
         
-        for k in range (int(top_left_track[0]), int(top_right_track[0]), int(interval_x)):
+        for k in range (int(top_left_track[0]), int(bottom_right_track[0]), int(interval_x)):
             cv2.line(frame, (k, int(top_left_track[1])), (k, int(bottom_right_track[1])), (255, 0, 0), 1, 1)
 
         for p in range (int(top_left_track[1]), int(bottom_right_track[1]), int(interval_y)):
-            cv2.line(frame, (int(top_left_track[0]), p), (int(top_right_track[0]), p), (255, 0, 0), 1, 1)
+            cv2.line(frame, (int(top_left_track[0]), p), (int(bottom_right_track[0]), p), (255, 0, 0), 1, 1)
 
         
         cv2.imshow("Frames", frame)
         count_frames += 1
-        cv2.imwrite("test_grid_"+str(count_frames)+".png", frame)
+        cv2.imwrite("Track/test_track_"+str(count_frames)+".jpg", frame)
         print('The GPS coordinates are: X -> ', actual_gps_x, ' || Y -> ', actual_gps_y)
         if count_frames == 1:
             coord = np.append(coord, [gps_x, gps_y])
@@ -208,25 +202,25 @@ while cap.isOpened():
             break
 
 print(coord)
-data = np.asarray(coord)
-data_trans = transformer(data)
 
-plt.figure()
-x1 = src[[0,1,2,3,0], 0]
-y1 = src[[0,1,2,3,0], 1]
-x2 = data.T[0]
-y2 = data.T[1]
-plt.plot(x1, y1, '-')
-plt.plot(x2, y2, 'o')
-plt.axis([min(x1),max(x1),max(y1),min(y1)])
-plt.grid(True)
-plt.figure()
-x1 = dst[[0,1,2,3,0], 0]
-y1 = dst[[0,1,2,3,0], 1]
-x2 = data_trans.T[0]
-y2 = data_trans.T[1]
-plt.plot(x1, y1, '-')
-plt.plot(x2, y2, 'o')
-plt.axis([min(x1),max(x1),max(y1),min(y1)])
-plt.grid(True)
-plt.show()
+# data = np.asarray(coord)
+# data_trans = transformer(data)
+# plt.figure()
+# x1 = src[[0,1,2,3,0], 0]
+# y1 = src[[0,1,2,3,0], 1]
+# x2 = data.T[0]
+# y2 = data.T[1]
+# plt.plot(x1, y1, '-')
+# plt.plot(x2, y2, 'o')
+# plt.axis([min(x1),max(x1),max(y1),min(y1)])
+# plt.grid(True)
+# plt.figure()
+# x1 = dst[[0,1,2,3,0], 0]
+# y1 = dst[[0,1,2,3,0], 1]
+# x2 = data_trans.T[0]
+# y2 = data_trans.T[1]
+# plt.plot(x1, y1, '-')
+# plt.plot(x2, y2, 'o')
+# plt.axis([min(x1),max(x1),max(y1),min(y1)])
+# plt.grid(True)
+# plt.show()
--- Server/GPS_RPI.py
+++ Server/GPS_RPI.py
@@ -11,8 +11,8 @@ from commlib.transports.mqtt import (
 class GPS:
     def __init__(self):
         ### Track Params ###
-        self.size_of_track_w = 160 #cm
-        self.size_of_track_h = 180 #cm
+        self.size_of_track_w = 480 #cm
+        self.size_of_track_h = 400 #cm
         self.init_x = 0 #cm
         self.init_y = 0 #cm
 
@@ -21,6 +21,8 @@ class GPS:
         self.width = 0
         self.interval_x = 0
         self.interval_y = 0
+        self.offset_x = 0
+        self.offset_y = 0
         self.points = np.zeros((5, 3))
         self.center_x = np.zeros(4)
         self.center_y = np.zeros(4)
@@ -67,7 +69,7 @@ class GPS:
     def detect_ArUco(self, frame, flag, points):
 
         correct_detection = False
-        (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, self.arucoDict, parameters=self.arucoParams)
+        (corners, ids, _) = cv2.aruco.detectMarkers(frame, self.arucoDict, parameters=self.arucoParams)
         #print("IDs: ", ids)
         cX = 0
         cY = 0
@@ -79,7 +81,7 @@ class GPS:
 
             # loop over the detected ArUCo corners
             for (markerCorner, markerID) in zip(corners, ids):
-                if flag == 0 and markerID != 8 and markerID != 0:
+                if flag == 0 and markerID != 10:
                     counter += 1
                     # marker corners are always returned in top-left, top-right, bottom-right and bottom-left order
                     corners = markerCorner.reshape((4, 2))
@@ -101,7 +103,7 @@ class GPS:
                     if counter == 3:
                         correct_detection = True
                 
-                elif flag == 1 and markerID == 8:
+                elif flag == 1 and markerID == 10:
                     counter += 1
                     # marker corners are always returned in top-left, top-right, bottom-right and bottom-left order
                     corners = markerCorner.reshape((4, 2))
@@ -138,21 +140,22 @@ class GPS:
 
             for i in range(0, 4):
                 index = [self.points[i][0], self.points[i][1]]
-                if self.points[i][2] == 1:
-                    top_left_track = index
-                    print("TL: ", top_left_track)
-                elif self.points[i][2] == 2:
-                    top_right_track = index
-                    print("TR: ", top_right_track)
-                elif self.points[i][2] == 3:
-                    bottom_right_track = index
-                    print("BR: ", bottom_right_track)
-                elif self.points[i][2] == 4:
-                    bottom_left_track = index
-                    print("BL: ", bottom_left_track)
+                if self.points[i][2] == 6:
+                    self.top_left_track = index
+                    print("TL: ", self.top_left_track)
+                elif self.points[i][2] == 7:
+                    self.top_right_track = index
+                    print("TR: ", self.top_right_track)
+                elif self.points[i][2] == 9:
+                    self.bottom_right_track = index
+                    print("BR: ", self.bottom_right_track)
+                elif self.points[i][2] == 8:
+                    self.bottom_left_track = index
+                    print("BL: ", self.bottom_left_track)
 
-            interval_x = 10*(top_right_track[0] - top_left_track[0])/self.size_of_track_w
-            interval_y = 10*(bottom_right_track[1] - top_left_track[1])/self.size_of_track_h
+            interval_x = 10*(self.bottom_right_track[0] - self.top_left_track[0])/self.size_of_track_w
+            interval_y = 10*(self.bottom_right_track[1] - self.top_left_track[1])/self.size_of_track_h
+
             cv2.imwrite("Initial_frame", frame_init)
 
         if countFrames >= 1:
@@ -162,17 +165,18 @@ class GPS:
             if point_car[0] > 0 and point_car[1] > 0:
                 gps_x = point_car[0] - self.top_left_track[0]
                 gps_y = point_car[1] - self.top_left_track[1]
-                actual_gps_x = self.init_x + (gps_x * self.size_of_track_w / (self.top_right_track[0] - self.top_left_track[0]))
-                actual_gps_y = self.init_y + (gps_y * self.size_of_track_h / (self.bottom_right_track[1] - self.top_left_track[1]))
+                actual_gps_x = self.offset_x + (gps_x * self.size_of_track_w / (self.bottom_right_track[0] - self.top_left_track[0]))
+                actual_gps_y = self.offset_y + (gps_y * self.size_of_track_h / (self.bottom_right_track[1] - self.top_left_track[1]))
                 cv2.putText(img, "X: "+str(int(actual_gps_x))+" & Y: "+str(int(actual_gps_y)), (int(gps_x), int(gps_y)), cv2.FONT_HERSHEY_SIMPLEX,
                                 1, (0, 0, 255), 3)
 
             
-            for k in range (int(self.top_left_track[0]), int(self.top_right_track[0]), int(self.interval_x)):
+            for k in range (int(self.top_left_track[0]), int(self.bottom_right_track[0]), int(interval_x)):
                 cv2.line(img, (k, int(self.top_left_track[1])), (k, int(self.bottom_right_track[1])), (255, 0, 0), 1, 1)
 
-            for p in range (int(self.top_left_track[1]), int(self.bottom_right_track[1]), int(self.interval_y)):
-                cv2.line(img, (int(self.top_left_track[0]), p), (int(self.top_right_track[0]), p), (255, 0, 0), 1, 1)
+            for p in range (int(self.top_left_track[1]), int(self.bottom_right_track[1]), int(interval_y)):
+                cv2.line(img, (int(self.top_left_track[0]), p), (int(self.bottom_right_track[0]), p), (255, 0, 0), 1, 1)
+
 
             cv2.imwrite("test_grid_"+str(countFrames)+".png", img)
             print('The GPS coordinates are: X -> ', actual_gps_x, ' || Y -> ', actual_gps_y)
@@ -180,9 +184,5 @@ class GPS:
                 self.coord = np.append(self.coord, [gps_x, gps_y])
             else:
                 self.coord = np.vstack((self.coord, [gps_x, gps_y]))
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                raise Exception("Stop imshow")
-
         
         return self.coord[len(self.coord)-1] 
