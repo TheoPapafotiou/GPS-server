e0656bf0b3f760537162012b37f6c8aaebb7026e
Papalight <theo.papafotiou@gmail.com>
2022-01-06 19:50:38 +0200

    .gitignore added + General file organizing

---
+++ Server_test/position_listener.py
@@ -0,0 +1,80 @@
+import sys
+sys.path.insert(0,'.')
+from threading import Thread
+import time
+import cv2
+from GPS_RPI import GPS
+from Merger import Merge_Files
+
+class PositionListener(Thread):
+    """PositionListener simulator aims to populate position variable. 
+    """
+    def __init__(self):
+
+        self.coor = None
+        self.i=1
+        self.j=1
+        
+        self.cap0 = GPS.video(0, 960, 540)
+        time.sleep(2)
+        self.cap2 = GPS.video(2, 960, 540)
+        time.sleep(2)
+
+        if self.cap0 is None:
+            print("Error in cap0")
+            self.cap0.release()
+        if self.cap2 is None:
+            print("Error in cap2")
+            self.cap2.release()
+
+        self.Merger = Merge_Files()
+        self.GPS = GPS()
+        self.__running = True
+        
+        Thread.__init__(self) 
+
+    ## Method for starting position listener simulation process.
+    #  @param self          The object pointer.
+    def start(self):
+        self.__running = True
+
+        super(PositionListener,self).start()
+
+    ## Method for stopping position listener simulation process.
+    #  @param self          The object pointer.
+    def stop(self):
+        self.__running = False
+        
+    ## Method for running position listener simulation process.
+    #  @param self          The object pointer.
+    def run(self):
+        """ 
+        Update coordinates every 0.1 seconds
+        """
+        countFrames = 0
+        while self.__running:
+            i = 0
+            j = 0
+            iX = 0
+            jX = 0
+            
+            start = time.time()
+            countFrames += 1
+            _, frame0 = self.cap0.read()
+            _, frame2 = self.cap2.read()
+
+            merged_frame = self.Merger.get_merged_frame(frame0, frame2)
+
+            i, j, iX, jX = self.GPS.tracking_procedure(merged_frame, countFrames)
+            self.coor = (i+15, j, iX+15, jX)
+            #print("Time for GPS: ", time.time() - start)
+
+            # Wait for 0.1 s before next adv
+    # 			print(20*"=")
+    # 			print("COUNT 10")
+    # 			print(20*"=")
+            time.sleep(0.1)
+        
+        self.cap0.release()
+        self.cap2.release()
+			
